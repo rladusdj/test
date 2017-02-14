605173a1dcfcd2f04fc7960f97bf5ea7a4443ea3
yeoneo kim <yeoneo@yeoneoui-MacBook-Pro.local>
2017-02-14 17:03:05 +0900

    Add New Message

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void Main(String [] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hi, professro");
+		System.out.println("My Name is yeoneo");
 	}
 
 }
